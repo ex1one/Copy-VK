eb77cc9d5370e30a4ea49633ee8fd69e6a722ff8
ex1one <vyvf11@gmail.com>
2022-05-25 19:29:57 +0500

    Made a markup for the sidebar and page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const Home = () => (
+  <div>
+    Home
+    <Button variant="contained">Contained</Button>
+  </div>
+);
+
+export default Home;
