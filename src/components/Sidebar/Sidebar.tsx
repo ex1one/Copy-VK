1a4e13528fa5119be2d24575378d3dec0846ad76
ex1one <vyvf11@gmail.com>
2022-05-25 19:51:03 +0500

    Basic structure sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import UserItem from '../UserItem/UserItem';
 
-const Sidebar = () => (
-  <div style={{ backgroundColor: 'red' }}>
-    dada
-  </div>
-);
+const Sidebar = () => {
+  const [da, setDa] = useState();
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <UserItem />
+    </Box>
+  );
+};
 
 export default Sidebar;
---
+++ src/components/UserItem/UserItem.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const UserItem = () => {
+  const [dada, setDada] = useState();
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Link to="/profile">
+        <Box sx={{ position: 'relative', marginRight: 5 }}>
+          <img src="" alt="" />
+          <Box sx={{
+            backgroundColor: 'green',
+            width: 4,
+            height: 4,
+            position: 'absolute',
+            bottom: '2',
+            left: '2',
+          }}
+          />
+        </Box>
+        <span>Имя</span>
+      </Link>
+    </Box>
+  );
+};
+
+export default UserItem;
