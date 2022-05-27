3cc0094a50cc887ffcdbb3e3d1b8d127e73ae59d
ex1one <vyvf11@gmail.com>
2022-05-27 20:30:57 +0500

    Connected FireBase

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material';
+import firebase from 'firebase/compat/app';
 import RootRoutes from './components/RootRoutes/RootRoutes';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBqOSCXyUvfnaGaQwBOXInT_THKo0BKYtE',
+  authDomain: 'copy-vk-47063.firebaseapp.com',
+  projectId: 'copy-vk-47063',
+  storageBucket: 'copy-vk-47063.appspot.com',
+  messagingSenderId: '536843072579',
+  appId: '1:536843072579:web:742111e802583c1d0190f6',
+});
 
 const div = document.createElement('div');
 if (document.body) document.body.append(div);
