90106e418bc239fb57410871bda8da28c18205c4
ex1one <vyvf11@gmail.com>
2022-05-28 18:06:20 +0500

    Made an online chat

--- src/pages/Messages/Messages.tsx
+++ src/pages/Messages/Messages.tsx
@@ -6,7 +6,6 @@ import {
   Fab,
   Grid, List,
   ListItem,
-  ListItemIcon,
   ListItemText,
   Paper,
   TextField,
@@ -24,81 +23,97 @@ const Messages = () => {
   const [error, setError] = useState<Error | null>(null);
   const [message, setMessage] = useState('');
 
-  const addMessage = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (user) {
-      try {
-        addDoc(collection(db, 'messages'), {
-          author: user,
-          createdAt: formatDate(new Date()),
-          message,
-        }).then();
-        setMessage('');
-      } catch (e) {
-        if (e instanceof Error) setError(e);
-      }
+  const addMessage = () => {
+    try {
+      addDoc(collection(db, 'messages'), {
+        user,
+        createdAt: formatDate(new Date()),
+        message,
+      }).then();
+      setMessage('');
+    } catch (e) {
+      if (e instanceof Error) setError(e);
+    }
+  };
+
+  const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addMessage();
     }
   };
 
   useEffect(() => {
-    const unSub = onSnapshot(collection(db, 'posts'), (doc) => {
+    const unSub = onSnapshot(collection(db, 'messages'), (doc) => {
+      const array:IMessage[] = [];
       doc.forEach((d) => {
         const post = d.data() as IMessage;
-        setMessages((prevState) => [post, ...prevState]);
+        array.push(post);
       });
+      setMessages(array);
     });
     return () => {
       unSub();
     };
   }, []);
+
   return (
     <>
       {error && <Alert severity="error">{error}</Alert>}
       <Grid container>
         <Grid item xs={12}>
-          <Typography variant="h5" className="header-message">Сообщения</Typography>
+          <Typography variant="h5" className={styles.title}>Сообщения</Typography>
         </Grid>
       </Grid>
       <Grid container component={Paper} className={styles.chatSection}>
         <Grid item xs={12}>
           <List className={styles.messageArea}>
-            <ListItem key="1">
-              <Grid container>
-                <Grid item xs={12}>
-                  <ListItemText sx={{ alignItem: 'right' }} primary="Hey man, What's up ?" />
-                </Grid>
-                <Grid item xs={12}>
-                  <ListItemText sx={{ alignItem: 'right' }} secondary="09:30" />
-                </Grid>
-              </Grid>
-            </ListItem>
-            <ListItem key="2">
-              <Grid container>
-                <Grid item xs={12}>
-                  <ListItemText sx={{ alignItem: 'left' }} primary="Hey, Iam Good! What about you ?" />
-                </Grid>
-                <Grid item xs={12}>
-                  <ListItemText sx={{ alignItem: 'left' }} />
-                </Grid>
-              </Grid>
-            </ListItem>
-            <ListItem key="3">
-              <Grid container>
-                <Grid item xs={12}>
-                  <ListItemText sx={{ alignItem: 'right' }} primary />
-                </Grid>
-                <Grid item xs={12}>
-                  <ListItemText sx={{ alignItem: 'right' }} />
-                </Grid>
-              </Grid>
-            </ListItem>
+            {messages.map((msg) => (
+              <>
+                <ListItem key={msg.createdAt}>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <ListItemText sx={msg.user._id === user?._id ? { color: '#19762d' } : {}} />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ListItemText sx={{ alignItem: 'right' }} />
+                    </Grid>
+                  </Grid>
+                </ListItem>
+
+                <ListItem key={msg.createdAt}>
+                  <Grid container sx={msg.user._id === user?._id ? { textAlign: 'right' } : {}}>
+                    <Grid display="flex" justifyContent={msg.user._id === user?._id ? 'flex-end' : 'flex-start'} item xs={12}>
+                      <Avatar src={msg.user.avatar} />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ListItemText sx={{ alignItem: 'right' }} secondary={msg.user.name} />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ListItemText sx={{ alignItem: 'right' }} primary={msg.message} />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ListItemText sx={{ alignItem: 'right' }} secondary={msg.createdAt} />
+                    </Grid>
+                  </Grid>
+                </ListItem>
+              </>
+            ))}
           </List>
           <Divider />
           <Grid className={styles.message} container>
             <Grid item xs={11}>
-              <TextField label="Напишите сообщение..." fullWidth />
+              <TextField
+                onKeyPress={changeHandler}
+                onChange={(event) => setMessage(event.target.value)}
+                label="Напишите сообщение..."
+                fullWidth
+                value={message}
+              />
             </Grid>
-            <Grid xs={1} className={styles.send}>
-              <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+            <Grid item xs={1} className={styles.send}>
+              <Fab onClick={addMessage} color="primary">
+                <SendIcon />
+              </Fab>
             </Grid>
           </Grid>
         </Grid>
